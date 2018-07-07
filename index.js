c7f420a1c12cdd9b313396e9e8c271903c6485be
shinik.d.cupo@gmail.com <shinik.d.cupo@gmail.com>
2018-07-07 21:47:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"}
   if (string.toUpperCase()=== string) {
     return "YES INDEED!"}
-  if (string === "I love you, Grandma."){
-    return "I love you,too."}
+  if (string === "I love you, Grandma.") {
+    return "I love you,too."
   }
+}
